55255cfb4afeac054250e49d4966d1d86082e863
AngelicXS <99211701+GouveiaXS@users.noreply.github.com>
2023-01-31 23:52:48 -0700

    Update client.lua

--- angelicxs_softcuffs/client.lua
+++ angelicxs_softcuffs/client.lua
@@ -3,10 +3,7 @@ local isDead, isSoftcuffed = false, false
 ESX = nil
 
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+	ESX = exports["es_extended"]:getSharedObject()
 
 
 end)
